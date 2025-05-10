5d5a9bea1b1b2691f48be1271cea2bc9e9914e94
Mike Catignani <mike@solutionsdeveloped.co.uk>
2025-05-10 18:53:21 +0100

    Fix session.user.id type for Vercel build

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,18 @@
+import NextAuth from 'next-auth';
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+      id?: string; // 👈 ADD THIS
+    };
+  }
+
+  interface User {
+    id: string;
+    name: string;
+    email: string;
+  }
+}

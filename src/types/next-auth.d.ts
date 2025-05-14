c0af664366e028b35bdf5d31f8c50322cb4d44bc
Mike Catignani <mike@solutionsdeveloped.co.uk>
2025-05-14 22:26:19 +0100

    V51 correction

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -7,19 +7,8 @@ declare module "next-auth" {
       name?: string | null;
       email?: string | null;
       image?: string | null;
-<<<<<<< HEAD
       id?: string;
       phone?: string; // ← Add this
     };
   }
-=======
-      id?: string; // 👈 ADD THIS
-      phone?: string
-    };
-  }
-
-  interface Session {
-    user?: User;
-  }
->>>>>>> 0f82c7484214cdb6a6bc13e5493a0600faa2ada9
 }
